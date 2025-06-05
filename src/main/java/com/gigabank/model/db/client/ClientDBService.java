e9ef2d3d17e375feec044647e9a5b7491e187662
OGShawnLee <OGShawnLee@gmail.com>
2025-06-04 23:41:57 -0600

    feat: add ClientDBProxy

---
+++ src/main/java/com/gigabank/model/db/client/ClientDBService.java
@@ -0,0 +1,54 @@
+package com.gigabank.model.db.client;
+
+import com.gigabank.model.data.ClientDTO;
+import com.gigabank.model.db.DBService;
+import com.gigabank.model.db.DuplicateRecordException;
+import com.gigabank.model.db.NotFoundRecordException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class ClientDBService extends DBService<HashMap<String, ClientDTO>> implements ClientDBServiceShape {
+  public ClientDBService() {
+    super("client", new HashMap<>());
+  }
+
+  @Override
+  public ClientDTO findOne(String displayName) {
+    return getDBStore().get(displayName);
+  }
+
+  @Override
+  public ClientDTO getOne(String displayName) throws NotFoundRecordException {
+    ClientDTO instance = getDBStore().get(displayName);
+
+    if (instance == null) {
+      throw new NotFoundRecordException("Employee with the given display name '" + displayName + "' not found.");
+    }
+
+    return instance;
+  }
+
+  @Override
+  public ArrayList<ClientDTO> getAll() {
+    return new ArrayList<>(getDBStore().values());
+  }
+
+  @Override
+  public ClientDTO createOne(ClientDTO employeeDTO) throws DuplicateRecordException, IOException {
+    if (getDBStore().containsKey(employeeDTO.getEmail())) {
+      throw new DuplicateRecordException("Employee with the given display name already exists.");
+    }
+
+    getDBStore().put(employeeDTO.getEmail(), employeeDTO);
+    writeToFile();
+    return employeeDTO;
+  }
+
+  @Override
+  public void deleteOne(String displayName) throws IOException {
+    getDBStore().remove(displayName);
+    writeToFile();
+  }
+}
