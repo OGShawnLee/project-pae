b3e32ba937dbc605c4e608240b5f15fcb66f0911
Tresdese <super_diegobros@hotmail.com>
2025-06-05 00:39:22 -0600

    Add employee list page

---
+++ src/main/java/com/gigabank/controller/ReviewEmployeeListController.java
@@ -0,0 +1,55 @@
+package com.gigabank.controller;
+
+import com.gigabank.model.data.EmployeeDTO;
+import com.gigabank.model.db.employee.EmployeeDBProxy;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+public class ReviewEmployeeListController extends Controller {
+    @FXML
+    private TableView<EmployeeDTO> tableEmployee;
+    @FXML
+    private TableColumn<EmployeeDTO, String> columnDisplayName;
+    @FXML
+    private TableColumn<EmployeeDTO, String> columnName;
+    @FXML
+    private TableColumn<EmployeeDTO, String> columnGender;
+    @FXML
+    private TableColumn<EmployeeDTO, Float> columnWage;
+    @FXML
+    private TableColumn<EmployeeDTO, String> columnEmail;
+
+    @FXML
+    private void initialize() {
+        loadTableColumns();
+        loadEmployeeList();
+    }
+
+    private void loadTableColumns() {
+        columnDisplayName.setCellValueFactory(new PropertyValueFactory<>("displayName"));
+        columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        columnGender.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        columnWage.setCellValueFactory(new PropertyValueFactory<>("wage"));
+        columnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+    }
+
+    private void loadEmployeeList() {
+        tableEmployee.setItems(
+                FXCollections.observableList(
+                        EmployeeDBProxy.getInstance().getAll()
+                )
+        );
+    }
+
+    public void handleOpenRegisterEmployee() {
+        Modal.display("Registrar Empleado", "RegisterEmployeeModal", this::loadEmployeeList);
+    }
+
+    public static void navigateToEmployeeListPage(Stage currentStage) {
+        navigateTo(currentStage, "Lista de Empleados", "ReviewEmployeeListPage");
+    }
+}
