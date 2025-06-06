5a9a1f9169d06a0c06308b3c2fe4e4cc90a608dd
Chema54 <josemariacm005@gmail.com>
2025-06-06 12:20:54 -0600

    fix: add create account and add role choicebox

--- src/main/java/com/gigabank/controller/RegisterEmployeeController.java
+++ src/main/java/com/gigabank/controller/RegisterEmployeeController.java
@@ -1,9 +1,11 @@
 package com.gigabank.controller;
 
+import com.gigabank.model.data.AccountDTO;
 import com.gigabank.model.data.EmployeeDTO;
 import com.gigabank.model.data.EmployeeDTO.EmployeeBuilder;
 import com.gigabank.model.data.Gender;
 import com.gigabank.model.db.DuplicateRecordException;
+import com.gigabank.model.db.account.AccountDBProxy;
 import com.gigabank.model.db.employee.EmployeeDBProxy;
 import com.gigabank.model.validation.InvalidFieldException;
 
@@ -12,6 +14,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.io.IOException;
+
+import static com.gigabank.model.data.AccountDTO.Role.EXECUTIVE;
+import static com.gigabank.model.data.AccountDTO.Role.TELLER;
 
 public class RegisterEmployeeController extends Controller {
     @FXML
@@ -33,30 +38,53 @@ public class RegisterEmployeeController extends Controller {
     private ChoiceBox<Gender> genderChoiceBox;
 
     @FXML
+    private ChoiceBox<AccountDTO.Role> roleChoiceBox;
+
+    @FXML
     private void initialize() {
         genderChoiceBox.getItems().addAll(Gender.values());
+        roleChoiceBox.getItems().addAll(EXECUTIVE, TELLER);
     }
 
     @FXML
     private void handleRegister() {
         try {
             String displayName = Validator.getValidDisplayName(displayNameField.getText());
-            EmployeeDTO duplicate = EmployeeDBProxy.getInstance().findOne(displayName);
+            AccountDTO duplicateAccount = AccountDBProxy.getInstance().findOne(displayName);
 
-            if (duplicate != null) {
-                throw new DuplicateRecordException("Ya existe un empleado con ese nombre de usuario.");
+            if (duplicateAccount != null) {
+                throw new DuplicateRecordException(
+                        "An account with this display name already exists. Please choose a different display name."
+                );
             }
 
-            EmployeeDTO employee = new EmployeeBuilder()
+            EmployeeDTO duplicateEmployee = EmployeeDBProxy.getInstance().findOne(displayName);
+
+            if (duplicateEmployee != null) {
+                throw new DuplicateRecordException(
+                        "An employee with this display name already exists. Please choose a different display name."
+                );
+            }
+
+            AccountDTO accountDTO = new AccountDTO(
+                    displayName,
+                    displayName + "@Password",
+                    AccountDTO.Role.valueOf(String.valueOf(roleChoiceBox.getValue()))
+            );
+
+
+            EmployeeDTO employeeDTO = new EmployeeBuilder()
                     .setDisplayName(displayNameField.getText())
                     .setName(nameField.getText())
                     .setAddress(addressField.getText())
                     .setBornAt(bornAtDatePicker.getValue())
+                    .setRole(roleChoiceBox.getValue())
                     .setGender(genderChoiceBox.getValue())
                     .setWage(wageField.getText())
                     .build();
 
-            EmployeeDBProxy.getInstance().createOne(employee);
+            AccountDBProxy.getInstance().createOne(accountDTO);
+            EmployeeDBProxy.getInstance().createOne(employeeDTO);
 
             Modal.displaySuccess("El empleado ha sido registrado exitosamente.");
         } catch (InvalidFieldException | DuplicateRecordException | IOException e) {
