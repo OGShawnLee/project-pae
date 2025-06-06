5af7e25b4ee7a8a93a4f48ca59c32f8d57e6f77f
Tresdese <super_diegobros@hotmail.com>
2025-06-06 02:17:35 -0600

    feat: delete register buttons from landing page

--- src/main/java/com/gigabank/controller/LandingAdministratorController.java
+++ src/main/java/com/gigabank/controller/LandingAdministratorController.java
@@ -13,14 +13,6 @@ public class LandingAdministratorController extends Controller {
     labelDisplayName.setText("@" + AuthClient.getInstance().getCurrentUser().getDisplayName());
   }
 
-  public void handleOpenRegisterBranch() {
-    Modal.display("Registrar Sucursal", "RegisterBranchModal");
-  }
-
-  public void handleOpenRegisterManager() {
-    Modal.display("Registrar Gerente", "RegisterManagerModal");
-  }
-
   public void handleLogOut() {
     AuthClient.getInstance().setCurrentUser(null);
     navigateFromThisPageTo("Login Page", "LoginPage");
